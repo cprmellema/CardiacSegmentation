f40488bc844e8a481acc0785d1076b4e1bc7e4fb
Cooper Mellema <cooper.mellema@utsouthwestern.edu>
2018-11-06 16:25:58 -0600

    utility files and necessary papers and nii interacting files

--- UtilityFunctions/CardiacSegmentation_Utilities.py
+++ UtilityFunctions/CardiacSegmentation_Utilities.py
@@ -54,22 +54,88 @@ class cPreprocess(object):
         self.ProcessedDataLocation = '/project/bioinformatics/DLLab/shared/Collab-Aashoo/WholeHeartSegmentation'
         self.TrainDataLocation = os.path.join(self.RawDataLocation, 'mr_train')
         self.TestDataLocation = os.path.join(self.ProcessedDataLocation, 'mr_test')
+        self.Dimension = 3 # 3 dimensional image
+
+        # Initialize the reference image that other images will be resampled based on
+        self.ReferenceImageParams = {'origin': np.zeros(self.Dimension), # sets the origin of the image to [0,0,0]
+                                     'direction': np.identity(self.Dimension).flatten(), # sets direction to [1,1,1] (arbitrary)
+                                     'size': [128]*self.Dimension, # downsample and/or upsample to 128x128x128
+                                     'spacing': np.ones(self.Dimension) # set spacing to 1mm (arbitrary selection)
+                                    }
+        self.ReferenceImage = sitk.Image(self.ReferenceImageParams['size'], 2)
+        self.ReferenceImage.SetOrigin(self.ReferenceImageParams['origin'])
+        self.ReferenceImage.SetSpacing(self.ReferenceImageParams['spacing'])
+        self.ReferenceImage.SetDirection(self.ReferenceImageParams['direction'])
 
     def fFetchRawDataFile(self, sPath):
-        NIIFile = sitk.ReadImage(os.path.join(sPath))
+        """Fetches a .nii file
+
+        (currently redundant with sitk.ReadImage, but will be changed later)
+
+        :param sPath: path to the .nii file
+        :return: the .nii file in the sitk object form
+        """
+        NIIFile = sitk.ReadImage(sPath)
         return NIIFile
 
-    def fNIIFileToArray(self, NIIFile):
-        aDerivedImg = sitk.Cast(NIIFile, sitk.sitkFloat32)
-        aDerivedImg = sitk.GetArrayViewFromImage(aDerivedImg)
+    def fResizeImage(self, NIIFile):
+        """ Resizes a .nii file to parameters set in self
+
+        :param NIIFile: .nii file to be set at origin, spacing, direction
+        :return:
+        """
+        # cResampler=sitk.ResampleImageFilter()
+        # cResampler.SetReferenceImage(self.ReferenceImage)
+        # NIIResampled=cResampler.execute(NIIFile)
+        NIIResampled=sitk.Resample(NIIFile, self.ReferenceImage)
+        return NIIResampled
+
+    def fNIIFileToNormArray(self, NIIFile, flStd=1, flMean=0):
+        """ Returns normalized array from the .nii file
+
+        array is normalized by subtracting the mean and dividing by the std,
+        if nothing is passed, just returns the array
+
+        :param NIIFile: Take in a .nii file
+        :return: returns an array from the .nii file
+        """
+        aDerivedImg = sitk.GetArrayFromImage(NIIFile)
+        aDerivedImg = aDerivedImg - flMean
+        aDerivedImg = np.divide(aDerivedImg, flStd)
         return aDerivedImg
 
-    def fFetchTrainingData(self, sNIIFileName):
+    def fFetchTrainingData(self, sNIIFileName, **Args):
+        """ Fetches Training data in raw form, normallizes it, and samples down to the same size
+        Uses a pipeline of this form:
+        Raw file -> fFetchRawDataFile -> imported raw nii file ->
+        fResizeImage -> resize and Resampled nii file ->
+        fNIIFileToNormArray -> normalized array
+
+        :param sNIIFileName: the file name of the .nii file
+        :return: array of normalized, resized data
+        """
         NIIFile = self.fFetchRawDataFile((os.path.join(self.TrainDataLocation, sNIIFileName)))
-        aDerivedImg = self.NIIFileToArray(NIIFile)
+        NIIFileResized = self.fResizeImage(NIIFile)
+        aDerivedImg = self.fNIIFileToNormArray(NIIFileResized, **Args)
         return aDerivedImg
 
     def fFetchTestData(self):
         pdTestData=pd.DataFrame
         return pdTestData
 
+Preprocesser=cPreprocess()
+
+for Root, Dirs, Files in os.walk(Preprocesser.TrainDataLocation):
+    Files.sort()
+    aUnNormalizedAll = np.zeros(np.append(len(Files), Preprocesser.ReferenceImageParams['size']))
+    for iFile, File in enumerate(Files):
+        aUnNormalizedAll[iFile, :, :, :] = Preprocesser.fFetchTrainingData(File)
+
+std = np.std(aUnNormalizedAll)
+mean = np.mean(aUnNormalizedAll)
+
+for Root, Dirs, Files in os.walk(Preprocesser.TrainDataLocation):
+    Files.sort()
+    aNormalizedAll = np.zeros(np.append(len(Files), Preprocesser.ReferenceImageParams['size']))
+    for iFile, File in enumerate(Files):
+        aNormalizedAll[iFile, :, :, :] = Preprocesser.fFetchTrainingData(File, flStd=std, flMean=mean)
